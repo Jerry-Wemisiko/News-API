bc51ecd8df675043f79495523f57b46fa97ed077
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-08-11 15:43:38 +0300

    install flask-script

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,11 @@
 from app.request import article_category, get_articles, get_source
-from flask import render_template
+from flask import render_template,request
 from . import main
+from ..request import get_articles,get_source,article_category
 from app import app
 
 #Views
-@app.route('/')
+@main.app.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
@@ -13,13 +14,13 @@ def index():
     newssource = get_source()
     return render_template('index.html',title = title,newssource= newssource)
 
-@app.route('/article/<id>')
+@main.app('/article/<id>')
 def article(id):
     
     articles = get_articles(id)
     return render_template('article.html',articles =articles,id = id)
 
-@app.route('category<name>')
+@main.app('category<name>')
 def category(name):
 
     category = article_category(name)
